71ba693dbb899ad84f6b45cfbd43c96ec770bdde
Edwar mantilla <edmantilla159@gmail.com>
2025-11-24 13:54:37 -0500

    Se realizo el ejercicio numero 5

---
+++ app5.js
@@ -0,0 +1,10 @@
+//Lista de tareas: Cree un arreglo vacío llamado tareas. Agregue tres actividades
+//utilizando push(). Luego elimine la última utilizando pop(). Muestre el resultado.
+
+let tareas = [];
+tareas.push("Lavar los platos");
+tareas.push("Tender la cama");
+tareas.push("Estudiar JavaScript");
+
+tareas.pop();
+console.log("Lista de tareas:", tareas);
