0e22fd03118a340db62b1f7a27d1ba7ddaf225f6
Edwar mantilla <edmantilla159@gmail.com>
2025-11-24 14:16:04 -0500

    Se realizo el ejercicio numero 8

---
+++ app8.js
@@ -0,0 +1,3 @@
+let libro = {titulo:"cien años de soledad", autor:"Gabriel Garcia Marquez", numero_paginas:300, genero:"Drama"};
+
+console.log("el libro", libro.titulo, "pertenece al autor", libro.autor, "y pertenece al genero ",libro.genero ," ademas  tiene", libro.numero_paginas, "paginas");
